d007396b80166542f65379034bc0238879b63440
jmkopper <jay.kopper@gmail.com>
2022-12-06 11:58:05 -0600

    Update day6.hs

--- day6/day6.hs
+++ day6/day6.hs
@@ -1,12 +1,8 @@
 module Main where
 import Data.List( nub )
 
-findMarker :: Eq a => [a] -> Int -> Int -> Int
-findMarker seq len index = if nub (take len seq) == take len seq
-    then index + len
-    else findMarker (drop 1 seq) len (index + 1)
-
 main :: IO ()
 main = do
     rawData <- readFile "input.txt"
-    print $ findMarker rawData 14 0
+    let n = 14
+    print $ head (filter (\y -> isMarker (take n (drop y rawData)) n) [1..length rawData - n]) + n
