e869efb15f16b2fb35e89ecf701fb45596868dfd
jmkopper <jay.kopper@gmail.com>
2022-12-08 22:47:39 -0600

    Update day8.rs

--- day8/day8.rs
+++ day8/day8.rs
@@ -1,39 +1,62 @@
-fn visible_from_start(trees: &Vec<u8>) -> Vec<u8> {
-    let mut tallest = trees[0];
-    let mut visible = vec![0u8; trees.len()];
-    visible[0] = 1;
-    for (i, tree) in trees[1..].iter().enumerate() {
-        if tree > &tallest {
-            tallest = *tree;
-            visible[i+1] = 1;
+fn scenic_score(row: usize, col: usize, trees: &Vec<Vec<u32>>) -> u32 {
+    let mut left_score = 1u32;
+    let mut right_score = 1u32;
+    let mut up_score = 1u32;
+    let mut down_score = 1u32;
+
+    let height = trees[row][col];
+
+    for i in 1..col+1 {
+        left_score = i as u32;
+        if trees[row][col-i] >= height {
+            break;
         }
     }
-    visible
+
+    for i in col+1..trees[0].len() {
+        right_score = (i-col) as u32;
+        if trees[row][i] >= height {
+            break;
+        }
+    }
+
+    for i in 1..row+1 {
+        up_score = i as u32;
+        if trees[row-i][col] >= height {
+            break;
+        }
+    }
+
+    for i in row+1..trees.len() {
+        down_score = (i-row) as u32;
+        if trees[i][col] >= height {
+            break
+        }
+    }
+
+    left_score * right_score * up_score * down_score
 }
 
-fn update_visible_row(visible: &mut Vec<Vec<u8>>, new_viz: &mut Vec<u8>, row: usize) {
-    for i in 0..new_viz.len() {
-        visible[row][i] = std::cmp::max(new_viz[i], visible[row][i]);
-    }
-}
-
-fn update_visible_col(visible: &mut Vec<Vec<u8>>, new_viz: &mut Vec<u8>, col: usize) {
-    for i in 0..new_viz.len() {
-        visible[i][col] = std::cmp::max(new_viz[i], visible[i][col]);
-    }
-}
 
 fn main() {
     let file_data = std::fs::read_to_string("input.txt").unwrap();
     let row_strs: Vec<&str> = file_data.trim().split("\n").collect();
-    let mut rows: Vec<Vec<u8>> = Vec::new();
-    for row_str in row_strs[..row_strs.len()-1].iter() {
+    let mut rows: Vec<Vec<u32>> = Vec::new();
+    for row_str in row_strs[..row_strs.len()].iter() {
         let digits: Vec<&str> = row_str.trim().split("").collect();
-        let digit_ints: Vec<u8> = digits[1..digits.len()-1].iter().map(|x| x.parse::<u8>().unwrap()).collect();
+        let digit_ints: Vec<u32> = digits[1..digits.len()-1].iter().map(|x| x.parse::<u32>().unwrap()).collect();
         rows.push(digit_ints);
     }
 
-    let mut visible = vec![vec![0u8; rows.len()]; rows[0].len()];
-    let tree_test: Vec<u8> = vec![1,2,3,2,1,4];
-    println!("{:?}", visible_from_start(&tree_test));
+    let mut scores = vec![vec![0u32; rows[0].len()]; rows.len()];
+    let mut max = 0u32;
+    for (i, row) in rows.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            let s = scenic_score(i, j, &rows);
+            scores[i][j] = s;
+            max = std::cmp::max(max, s);
+        }
+    }
+    
+    println!("{:?}", max);
 }
