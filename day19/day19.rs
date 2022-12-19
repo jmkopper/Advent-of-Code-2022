dd9b81f9ecfebbccea4d68a92de9c4f722ee2705
jmkopper <jay.kopper@gmail.com>
2022-12-19 16:32:46 -0600

    Update day19.rs

--- day19/day19.rs
+++ day19/day19.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::{HashSet, VecDeque, HashMap},
+    hash::Hash,
+};
+
 #[derive(Debug)]
 struct Cost {
     ore_cost: i64,
@@ -12,9 +17,12 @@ struct Blueprint {
     clay_bot_cost: Cost,
     obs_bot_cost: Cost,
     geode_bot_cost: Cost,
+    max_ore: i64,
+    max_clay: i64,
+    max_obs: i64
 }
 
-#[derive(Clone, Copy, PartialEq, Hash, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 struct State {
     ore_bots: i64,
     clay_bots: i64,
@@ -28,69 +36,66 @@ struct State {
 }
 
 fn neighbors(state: &State, blueprint: &Blueprint) -> Vec<State> {
-    let mut neighbors: Vec<State> = Vec::new();
-    if state.ore_bots * state.time_remaining >= blueprint.ore_bot_cost.ore_cost {
-        let mut next_ore = state.clone();
-        let tick = blueprint.ore_bot_cost.ore_cost - state.ore + 1;
-        next_ore.ore_bots = state.ore_bots + 1;
-        next_ore.ore = state.ore - blueprint.ore_bot_cost.ore_cost + tick * state.ore_bots;
-        next_ore.clay = state.clay + tick * state.clay_bots;
-        next_ore.obsidian = state.obsidian + tick * state.obs_bots;
-        next_ore.geodes = state.geodes + tick * state.geode_bots;
-        next_ore.time_remaining = state.time_remaining - tick;
-        neighbors.push(next_ore);
-    }
-    if state.ore_bots * state.time_remaining >= blueprint.clay_bot_cost.ore_cost {
-        let mut next_clay = state.clone();
-        let tick = blueprint.clay_bot_cost.ore_cost - state.ore + 1;
-        next_clay.clay_bots = state.clay_bots + 1;
-        next_clay.ore = state.ore - blueprint.clay_bot_cost.ore_cost + tick * state.ore_bots;
-        next_clay.clay = state.clay + tick * state.clay_bots;
-        next_clay.obsidian = state.obsidian + tick * state.obs_bots;
-        next_clay.geodes = state.geodes + tick * state.geode_bots;
-        next_clay.time_remaining = state.time_remaining - tick;
-        neighbors.push(next_clay);
-    }
-    if state.ore_bots * state.time_remaining >= blueprint.obs_bot_cost.ore_cost
-        && state.clay_bots * state.time_remaining >= blueprint.obs_bot_cost.clay_cost
-    {
-        let mut next_obsidian = state.clone();
-        let tick = std::cmp::max(
-            blueprint.obs_bot_cost.ore_cost - state.ore,
-            blueprint.obs_bot_cost.clay_cost - state.clay,
-        ) + 1;
-        next_obsidian.obs_bots = state.obs_bots + 1;
-        next_obsidian.ore = state.ore - blueprint.obs_bot_cost.ore_cost + tick * state.ore_bots;
-        next_obsidian.clay = state.clay - blueprint.obs_bot_cost.clay_cost + tick * state.clay_bots;
-        next_obsidian.obsidian = state.obsidian + tick * state.obs_bots;
-        next_obsidian.geodes = state.geodes + tick * state.geode_bots;
-        next_obsidian.time_remaining = state.time_remaining - tick;
-        neighbors.push(next_obsidian);
-    }
-    if state.ore_bots * state.time_remaining >= blueprint.geode_bot_cost.ore_cost
-        && state.obs_bots * state.time_remaining >= blueprint.geode_bot_cost.obsidian_cost
-    {
-        let mut next_geode = state.clone();
-        let tick = std::cmp::max(
-            blueprint.geode_bot_cost.ore_cost - state.ore,
-            blueprint.geode_bot_cost.obsidian_cost - state.obsidian,
-        ) + 1;
-        next_geode.geode_bots = state.geode_bots + 1;
-        next_geode.ore = state.ore - blueprint.geode_bot_cost.ore_cost + tick * state.ore_bots;
-        next_geode.obsidian = state.obsidian - blueprint.geode_bot_cost.obsidian_cost + tick * state.obs_bots;
-        next_geode.clay = state.clay - blueprint.geode_bot_cost.clay_cost + tick * state.clay_bots;
-        next_geode.geodes = state.geodes + tick * state.geode_bots;
-        next_geode.time_remaining = state.time_remaining - tick;
-        neighbors.push(next_geode);
+    if state.time_remaining <= 0 {
+        return vec![];
     }
 
-    if state.time_remaining > 0 && neighbors.is_empty() {
-        let mut run_out_the_clock = state.clone();
-        run_out_the_clock.ore = state.ore + state.time_remaining * state.ore_bots;
-        run_out_the_clock.clay = state.clay + state.time_remaining * state.clay_bots;
-        run_out_the_clock.obsidian = state.obsidian + state.time_remaining * state.obs_bots;
-        run_out_the_clock.geodes = state.geodes + state.time_remaining * state.geode_bots;
+    if state.ore >= blueprint.geode_bot_cost.ore_cost
+        && state.obsidian >= blueprint.geode_bot_cost.obsidian_cost
+    {
+        let mut buy_geode_bot = state.clone();
+        buy_geode_bot.geode_bots = state.geode_bots + 1;
+        buy_geode_bot.ore = state.ore - blueprint.geode_bot_cost.ore_cost + state.ore_bots;
+        buy_geode_bot.obsidian =
+            state.obsidian - blueprint.geode_bot_cost.obsidian_cost + state.obs_bots;
+        buy_geode_bot.clay = state.clay + state.clay_bots;
+        buy_geode_bot.geodes = state.geodes + state.geode_bots;
+        buy_geode_bot.time_remaining -= 1;
+        return vec![buy_geode_bot];
     }
+
+    let mut neighbors: Vec<State> = Vec::new();
+    if state.ore >= blueprint.ore_bot_cost.ore_cost  && state.ore_bots < blueprint.max_ore {
+        let mut buy_ore_bot = state.clone();
+        buy_ore_bot.ore_bots = state.ore_bots + 1;
+        buy_ore_bot.ore = state.ore - blueprint.ore_bot_cost.ore_cost + state.ore_bots;
+        buy_ore_bot.clay = state.clay + state.clay_bots;
+        buy_ore_bot.obsidian = state.obsidian + state.obs_bots;
+        buy_ore_bot.geodes = state.geodes + state.geode_bots;
+        buy_ore_bot.time_remaining -= 1;
+        neighbors.push(buy_ore_bot);
+    }
+    if state.ore >= blueprint.clay_bot_cost.ore_cost && state.clay_bots < blueprint.max_clay {
+        let mut buy_clay_bot = state.clone();
+        buy_clay_bot.clay_bots = state.clay_bots + 1;
+        buy_clay_bot.ore = state.ore - blueprint.clay_bot_cost.ore_cost + state.ore_bots;
+        buy_clay_bot.clay = state.clay + state.clay_bots;
+        buy_clay_bot.obsidian = state.obsidian + state.obs_bots;
+        buy_clay_bot.geodes = state.geodes + state.geode_bots;
+        buy_clay_bot.time_remaining -= 1;
+        neighbors.push(buy_clay_bot);
+    }
+    if state.ore >= blueprint.obs_bot_cost.ore_cost
+        && state.clay >= blueprint.obs_bot_cost.clay_cost && state.obs_bots < blueprint.max_obs
+    {
+        let mut buy_obs_bot = state.clone();
+        buy_obs_bot.obs_bots = state.obs_bots + 1;
+        buy_obs_bot.ore = state.ore - blueprint.obs_bot_cost.ore_cost + state.ore_bots;
+        buy_obs_bot.clay = state.clay - blueprint.obs_bot_cost.clay_cost + state.clay_bots;
+        buy_obs_bot.obsidian = state.obsidian + state.obs_bots;
+        buy_obs_bot.geodes = state.geodes + state.geode_bots;
+        buy_obs_bot.time_remaining -= 1;
+        neighbors.push(buy_obs_bot);
+    }
+
+    let mut wait = state.clone();
+    wait.ore = state.ore + state.ore_bots;
+    wait.clay = state.clay + state.clay_bots;
+    wait.obsidian = state.obsidian + state.obs_bots;
+    wait.geodes = state.geodes + state.geode_bots;
+    wait.time_remaining -= 1;
+    neighbors.push(wait);
+
     return neighbors;
 }
 
@@ -118,31 +123,51 @@ fn parse_line(line: &str) -> Blueprint {
         clay_cost: 0,
         obsidian_cost: words[30].parse().unwrap(),
     };
+    let &max_ore = vec![ore_bot_cost.ore_cost, clay_bot_cost.ore_cost, obsidian_bot_cost.ore_cost, geode_bot_cost.ore_cost].iter().max().unwrap();
+    let max_clay = obsidian_bot_cost.clay_cost;
+    let max_obs = geode_bot_cost.obsidian_cost;
     Blueprint {
         id,
         ore_bot_cost,
         clay_bot_cost,
         obs_bot_cost: obsidian_bot_cost,
         geode_bot_cost,
+        max_ore,
+        max_clay,
+        max_obs,
     }
 }
 
-fn traverse_blueprint(state: &State, blueprint: &Blueprint) -> Vec<State> {
-    if state.time_remaining == 0 {
-        return vec![*state];
-    } else if state.time_remaining < 0 {
-        return vec![];
-    }
-    let mut nodes: Vec<State> = Vec::new();
-    for neighbor in neighbors(state, blueprint).iter() {
-        nodes.extend(traverse_blueprint(neighbor, blueprint));
+fn bfs_blueprint(state: &State, blueprint: &Blueprint) -> i64 {
+    let mut queue: VecDeque<State> = VecDeque::new();
+    let mut parents: HashSet<State> = HashSet::new();
+    let mut cache: HashMap<i64, i64> = HashMap::new();
+    for i in 0..=state.time_remaining {
+        cache.insert(i, 0);
     }
 
-    return nodes;
+    queue.push_back(*state);
+    while queue.len() > 0 && state.time_remaining >= 0 {
+        let v = queue.pop_front().unwrap();
+        if v.geodes < *cache.get(&v.time_remaining).unwrap() {
+            continue;
+        }
+
+        cache.insert(v.time_remaining, v.geodes);
+
+        for n in neighbors(&v, blueprint).into_iter() {
+            if !parents.contains(&n) {
+                parents.insert(n);
+                queue.push_back(n);
+            }
+        }
+    }
+
+    *cache.get(&0).unwrap()
 }
 
 fn main() {
-    let raw_data = std::fs::read_to_string("test.txt").unwrap();
+    let raw_data = std::fs::read_to_string("input.txt").unwrap();
     let lines: Vec<&str> = raw_data.split("\n").filter(|x| !x.is_empty()).collect();
     let blueprints: Vec<Blueprint> = lines.iter().map(|x| parse_line(x)).collect();
 
@@ -157,12 +182,14 @@ fn main() {
         geodes: 0,
         time_remaining: 24,
     };
-    let bp1 = traverse_blueprint(&init_state, &blueprints[0]);
-    let mut best = 0;
-    for state in bp1.iter() {
-        if state.geodes > best {
-            println!("{:?}", state);
-            best = state.geodes
-        }
-    }
+
+    let mut init_state_pt2 = init_state.clone();
+    init_state_pt2.time_remaining = 32;
+
+    let quality_levels: Vec<i64> = blueprints.iter().map(|b| bfs_blueprint(&init_state, b)*b.id).collect();
+    println!("part 1: {:?}", quality_levels.iter().sum::<i64>());
+
+    let uneaten_blueprints: Vec<&Blueprint> = blueprints.iter().take(3).collect();
+    let geodes: Vec<i64> = uneaten_blueprints.iter().map(|b| bfs_blueprint(&init_state_pt2, b)).collect();
+    println!("part 2: {:?}", geodes[0]*geodes[1]*geodes[2]);
 }
