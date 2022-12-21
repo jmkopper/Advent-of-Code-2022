805565b7cc7fde5403d3150da9f23ee52a5b8618
jmkopper <jay.kopper@gmail.com>
2022-12-21 10:56:00 -0600

    Update day21.rs

--- day21/day21.rs
+++ day21/day21.rs
@@ -6,14 +6,7 @@ enum Op {
     Sub,
     Div,
     Mult,
-    Eq,
     Just(i64),
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Output {
-    Num(i64),
-    Truth(bool),
 }
 
 #[derive(Debug)]
@@ -37,11 +30,6 @@ fn parse_line(line: &str) -> Node {
             _ => Op::Add,
         }
     };
-    // let op = if name == "root" {
-    //     Op::Eq
-    // } else {
-    //     op
-    // };
     let children: Option<(String, String)> = if words.len() == 2 {
         None
     } else {
@@ -51,27 +39,26 @@ fn parse_line(line: &str) -> Node {
     return Node { name, children, op }
 }
 
-fn do_op(op: Op, fst: i64, snd: i64) -> Output {
+fn do_op(op: Op, fst: i64, snd: i64) -> i64 {
     match op {
-        Op::Add => Output::Num(fst + snd),
-        Op::Sub => Output::Num(fst - snd),
-        Op::Mult => Output::Num(fst * snd),
-        Op::Div => Output::Num(fst / snd),
-        Op::Just(x) => Output::Num(x),
-        Op::Eq => Output::Truth(fst == snd)
+        Op::Add => fst + snd,
+        Op::Sub => fst - snd,
+        Op::Mult => fst * snd,
+        Op::Div => fst / snd,
+        Op::Just(x) => x,
     }
 }
 
-fn eval(node: &Node, graph: &HashMap<String, Node>, values: &mut HashMap<String, Output>) -> Output {
+fn eval(node: &Node, graph: &HashMap<String, Node>, values: &mut HashMap<String, i64>) -> i64 {
     if let Some(&x) = values.get(&node.name) {
         return x;
     }
     if let Op::Just(x) = node.op {
-        return Output::Num(x);
+        return x;
     }
     let left = &node.children.as_ref().unwrap().0;
     let right = &node.children.as_ref().unwrap().1;
-    if let (Some(&Output::Num(fst)), Some(&Output::Num(snd))) = (values.get(left), values.get(right)) {
+    if let (Some(&fst), Some(&snd)) = (values.get(left), values.get(right)) {
         let v = do_op(node.op, fst, snd);
         values.insert(node.name.to_string(), v);
         return v;
@@ -79,12 +66,12 @@ fn eval(node: &Node, graph: &HashMap<String, Node>, values: &mut HashMap<String,
 
     // recurse
     let left_child = graph.get(left).unwrap();
+    let fst = eval(left_child, graph, values);
     let right_child = graph.get(right).unwrap();
-    if let (Output::Num(fst), Output::Num(snd)) = (eval(left_child, graph, values), eval(right_child, graph, values)) {
-        let v = do_op(node.op, fst, snd);
-        values.insert(node.name.to_string(), v);
-    }
-    return Output::Truth(false);
+    let snd = eval(right_child, graph, values);
+    let v = do_op(node.op, fst, snd);
+    values.insert(node.name.to_string(), v);
+    return v;
 }
 
 fn main() {
@@ -92,7 +79,7 @@ fn main() {
     let lines: Vec<&str> = raw_data.split("\n").filter(|x| !x.is_empty()).collect();
     let nodes: Vec<Node> = lines.iter().map(|x| parse_line(x)).collect();
     let graph: HashMap<String, Node> = HashMap::from_iter(nodes.into_iter().map(|x| (x.name.to_string(), x)));
-    let mut values: HashMap<String, Output> = HashMap::new();
+    let mut values: HashMap<String, i64> = HashMap::new();
     let root = graph.get(&"root".to_string()).unwrap();
     println!("{:?}", eval(root, &graph, &mut values));
 }
