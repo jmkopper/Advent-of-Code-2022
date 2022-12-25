8249c4503fe0fc63fd16c41dffdce5fd5f622c1d
jmkopper <jay.kopper@gmail.com>
2022-12-25 12:15:19 -0600

    Create day25.py

---
+++ day25/day25.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+
+SNAFU = {'1': 1, '2': 2, '0': 0, '-': -1, '=': -2}
+SNAFU_INV = {0: ('0', 0), 1: ('1', 0), 2: ('2', 0), 3: ('=', 1), 4: ('-', 1), 5: ('0', 1)}
+
+def to_dec(s):
+    return sum([SNAFU[c] * (5**i) for i, c in enumerate(s[::-1])])
+
+def to_snafu(n):
+    snafu = ''
+    extra = 0
+    while n > 0:
+        symbol, extra = SNAFU_INV[n % 5 + extra]
+        snafu = symbol + snafu
+        n //= 5
+    if extra:
+        snafu = SNAFU_INV[extra][0] + snafu
+    return snafu
+
+def main():
+    with open('input.txt') as f:
+        data = f.readlines()
+
+    lines = [line.strip() for line in data]
+    print(to_snafu(sum([to_dec(line) for line in lines])))
+
+if __name__ == '__main__':
+    main()
